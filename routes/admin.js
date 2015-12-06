ff0dc82d5a6ba0b85b05a269868d82ca5b80bff9
Amanda Bausch <manadab@me.com>
2015-12-05 23:16:36 -0600

    still not getting data on the admin page

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('../data/mongo');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render( 'admin.jade');
-    console.log('i am here on admin');
+    res.render('admin.jade');
+   // var results = mongo.applications.find({'_id': parseInt(req.params.id)}).limit(1);
+    //var array = results.toArray(function(err, arr) {
+     //   var doc = array[0];
+       // res.json(doc);
+
+   // })
+
 });
 
 module.exports = router;
-
