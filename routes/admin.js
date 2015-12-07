cc62340a6461f9634a75aa7ed838724220aa231e
Amanda Bausch <manadab@me.com>
2015-12-06 18:05:58 -0600

    tables in progress

--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,7 @@ var mongo = require('../data/mongo');
 /* GET home page. */
 router.get('/', function(req, res) {
     //pull the data from the database
-    var results = mongo.applications.find().toArray(function(err, array) {
+    var results = mongo.applications.find().limit(5).toArray(function(err, array) {
 
       res.render('admin.jade', {data: array});
 
